ddff9504f00a1dd4de319eef73cf38c931d467d1
Harsh Tripathi <harshcooltripathi1@gmail.com>
2026-05-02 13:53:24 +0530

    movie site

--- Lec16/MovieProject/app.js
+++ Lec16/MovieProject/app.js
@@ -7,8 +7,11 @@ const path= require('path');
 const mongoose=require('mongoose');
 const seed=require('./seed')
 const Movie=require('./models/movies');
+const methodOverride = require('method-override');
 
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended:true}))
+app.use(methodOverride('_method'));
 
 // mongoose.connect('app.use(express.urlencoded({extended:true}));')
 mongoose.connect('mongodb://127.0.0.1:27017/Movies')
@@ -23,6 +26,36 @@ app.get('/',async(req,res)=>{
     // res.send(allMovies);
 
     res.render('index.ejs',{allMovies})
+})
+
+app.get('/new', (req,res)=>{
+    res.render('new.ejs')
+})
+
+app.post('/create',async(req,res)=>{
+  const {name,year,img,desc}=req.body;
+
+//   console.log(name,year,img,desc);
+
+  await Movie.insertOne({name,year,img,desc});
+
+  res.redirect('/');
+
+})
+
+app.get('/movies/:id',async (req,res)=>{
+    const {id}=req.params;
+    const m=await Movie.findById(id);
+    console.log(m);
+    res.render('show.ejs',{m})
+})
+
+app.delete('/movie/delete/:id',async (req,res)=>{
+    const {id}=req.params;
+
+    await Movie.findByIdAndDelete(id);
+
+    res.redirect('/')
 })
 
 // seed();
