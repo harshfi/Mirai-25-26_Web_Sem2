0cb41aad9b042df769376323157bbe0414e80c4b
Harsh Tripathi <harshcooltripathi1@gmail.com>
2026-05-04 11:57:49 +0530

    rating & edit in movie

---
+++ Lec17/MovieProject/app.js
@@ -0,0 +1,100 @@
+const express =require('express');
+
+const app= express();
+
+const path= require('path');
+
+const mongoose=require('mongoose');
+const seed=require('./seed')
+const Movie=require('./models/movies');
+const methodOverride = require('method-override');
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended:true}))
+app.use(methodOverride('_method'));
+
+// mongoose.connect('app.use(express.urlencoded({extended:true}));')
+mongoose.connect('mongodb://127.0.0.1:27017/Movies')
+.then(()=>{
+    console.log('DB connected');
+})
+// seed();
+
+app.get('/',async(req,res)=>{
+    const allMovies= await Movie.find();
+
+    // res.send(allMovies);
+
+    res.render('index.ejs',{allMovies})
+})
+
+app.get('/new', (req,res)=>{
+    res.render('new.ejs')
+})
+
+app.post('/create',async(req,res)=>{
+  const {name,year,img,desc}=req.body;
+
+//   console.log(name,year,img,desc);
+
+  await Movie.insertOne({name,year,img,desc});
+
+  res.redirect('/');
+
+})
+
+app.get('/movies/:id',async (req,res)=>{
+    const {id}=req.params;
+    const m=await Movie.findById(id);
+    console.log(m);
+    res.render('show.ejs',{m})
+})
+
+app.delete('/movie/delete/:id',async (req,res)=>{
+    const {id}=req.params;
+
+    await Movie.findByIdAndDelete(id);
+
+    res.redirect('/')
+})
+
+app.get('/edit/:id',async(req,res)=>{
+    const {id}=req.params;
+    const m=await Movie.findById(id);
+
+    res.render('edit.ejs',{m});
+})
+
+
+app.put('/edit/:id',async(req,res)=>{
+    const {id}=req.params;
+
+    const {name,year,img,desc}=req.body;
+
+    await Movie.findByIdAndUpdate(id,{name,year,img,desc});
+
+    res.redirect(`/movies/${id}`);
+
+})
+
+
+app.post('/rating/:id',async(req,res)=>{
+    const {id}=req.params;
+    const {rating,comment}=req.body;
+
+    const m= await Movie.findById(id);
+    m.reviews.push({rating,comment});
+    await m.save();
+
+    console.log(m);
+
+    res.redirect(`/movies/${id}`);
+})
+
+
+
+
+
+app.listen(3000,()=>{
+    console.log('server has started')
+})
---
+++ Lec17/MovieProject/models/movies.js
@@ -0,0 +1,36 @@
+const mongoose= require('mongoose');
+
+const movieSchema= new mongoose.Schema({
+    name:{
+        type:String,
+        trim:true,
+        required:true
+    },
+    year:Number,
+    img:{
+        type:String,
+        // required:true,
+        default:"https://thumbs.dreamstime.com/b/movie-night-retro-poster-design-cinema-popcorn-graphic-film-strip-entertainment-brochure-template-97475695.jpg"
+
+    },
+    desc:{
+        type:String,
+        
+    },
+    reviews:[{
+       rating:{
+       type: Number,
+       min:0,
+       max:5
+       },
+       comment:{
+         type:String
+       }
+       
+    }]
+})
+
+
+const Movie=mongoose.model('Movie',movieSchema);
+
+module.exports=Movie;
